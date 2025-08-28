81d8ddd3e41fbfec3b582f83c9f4814284d57a4d
Santiago Chicaiza <santiagochicaiza@yahoo.es>
2025-08-28 00:11:19 -0500

    Se elimina archivos

--- parking-frontend/src/components/layoutMaps/LayoutMap3.js
+++
@@ -1,9 +0,0 @@
-﻿
-
-import RenderSpot from "../commons/RenderSpot";
-
-export default function LayoutMap3({ spots }) {
-    return (
-        <RenderSpot spots={spots} cols={12} rows={9} />
-    );
-}
--- parking-frontend/src/components/parking/ParkingDetailModal.js
+++ parking-frontend/src/components/parking/ParkingDetailModal.js
@@ -1,30 +1,20 @@
 ﻿import React, { useEffect, useState } from 'react';
 import { Dialog } from 'primereact/dialog';
 
-import { parkingSpotsMap1 as initialSpots } from '../../utils/parkingSpotsMap1';
-import { parkingSpotsMap2 as initialSpots2 } from '../../utils/parkingSpotsMap2';
-import { parkingSpotsMap3 as initialSpots3 } from '../../utils/parkingSpotsMap3';
 import LayoutMap from '../layoutMaps/LayoutMap';
-import LayoutMap1 from '../layoutMaps/LayoutMap1';
-import LayoutMap2 from '../layoutMaps/LayoutMap2';
-import LayoutMap3 from '../layoutMaps/LayoutMap3';
 import { fetchParkingsSpots } from '../../api/parkingSpots';
 
 export default function ParkingDetailModal({ visible, onHide, parking }) {
 
 
-    const [spots, setSpots] = useState(initialSpots);
-    const [spots2, setSpots2] = useState(initialSpots2);
-    const [spots3, setSpots3] = useState(initialSpots3);
+    const [spots, setSpots] = useState([]);
 
 
     useEffect(() => {
         fetchParkingsSpots(parking?.floorId).then((spots) => {
             setSpots(spots.cells || []);
         });
-    }, [spots]);
-
-    // if (!parking) return null;
+    }, [parking?.floorId]);
 
 
     return (
@@ -39,8 +29,6 @@ export default function ParkingDetailModal({ visible, onHide, parking }) {
             modal
             dismissableMask
         >
-            {/*<LayoutMap1 spots={spots} />
-            <LayoutMap2 spots={spots2} />*/}
             <LayoutMap spots={spots} />
         </Dialog>
 
